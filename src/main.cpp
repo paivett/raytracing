3458dd93f6eb797f2590af9fbc6bc23896ec0dbb
paivett <santiago.pivetta@gmail.com>
2018-09-06 21:15:40 -0300

    First sphere onscreen!

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,21 @@
 using namespace std;
 
 
+bool hit_sphere(const Vec3& center, float radius, const Ray& ray) {
+    Vec3 oc = ray.origin() - center;
+    float a = ray.direction() * ray.direction();
+    float b = 2.0 * oc * ray.direction();
+    float c = (oc * oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    return discriminant > 0.0;
+}
+
+
 Vec3 compute_color(const Ray& ray) {
+    if (hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, ray)) {
+        return Vec3(1.0, 0.0, 0.0);
+    }
+
     Vec3 unit_direction = ray.direction().normalized();
     float t = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0);
@@ -14,8 +28,8 @@ Vec3 compute_color(const Ray& ray) {
 
 
 int main() {
-    int nx = 200;
-    int ny = 100;
+    int nx = 400;
+    int ny = 200;
 
     cout << "P3" << endl << nx << " " << ny << endl << "255" << endl;
     
