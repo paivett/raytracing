034a4d0a9eeb1bb8768576750be104d71d8cc0d3
paivett <santiago.pivetta@gmail.com>
2018-09-10 22:29:45 -0300

    Generate random scene

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,39 @@ Vec3 compute_color(const Ray& ray, const HitableList& world, int recursion_depth
     return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0);
 }
 
+HitableList random_scene() {
+    int n = 500;
+    HitableList world;
+    world.add(make_shared<Sphere>(Vec3(0.0, -1000.0, 0.0), 1000.0, make_shared<Lambertian>(Vec3(0.5, 0.5, 0.5))));
+
+    for (int a = -11; a < 11; ++a) {
+        for (int b = -11; b < 11; ++b) {
+            float choose_material = random_number();
+            Vec3 center = Vec3(a + 0.9 * random_number(), 0.2, b + 0.9 * random_number());
+            if ((center - Vec3(4.0, 0.2, 0.0)).norm() > 0.9) {
+                if (choose_material < 0.8) {
+                    Vec3 albedo = Vec3(random_number() * random_number(), random_number() * random_number(), random_number() * random_number());
+                    world.add(make_shared<Sphere>(center, 0.2, make_shared<Lambertian>(albedo)));
+                }
+                else if (choose_material < 0.95) {
+                    Vec3 albedo = 0.5 * (Vec3(random_number(), random_number(), random_number()) + Vec3(1.0, 1.0, 1.0));
+                    float fuzziness = random_number() * 0.5;
+                    world.add(make_shared<Sphere>(center, 0.2, make_shared<Metal>(albedo, fuzziness)));
+                }
+                else {
+                    world.add(make_shared<Sphere>(center, 0.2, make_shared<Dielectric>(1.5)));                
+                }
+            }
+        }
+    }
+
+    world.add(make_shared<Sphere>(Vec3(0.0, 1.0, 0.0), 1.0, make_shared<Dielectric>(1.5)));
+    world.add(make_shared<Sphere>(Vec3(-4.0, 1.0, 0.0), 1.0, make_shared<Lambertian>(Vec3(0.4, 0.2, 0.1))));
+    world.add(make_shared<Sphere>(Vec3(4.0, 1.0, 0.0), 1.0, make_shared<Metal>(Vec3(0.7, 0.6, 0.5), 0.0)));
+
+    return world;
+}
+
 int main() {
     int nx = 400;
     int ny = 200;
@@ -46,17 +79,12 @@ int main() {
     shared_ptr<Sphere> s4 = make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.5, make_shared<Dielectric>(1.5));
     shared_ptr<Sphere> s5 = make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), -0.45, make_shared<Dielectric>(1.5));
 
-    HitableList world;
-    world.add(s1);
-    world.add(s2);
-    world.add(s3);
-    world.add(s4);
-    world.add(s5);
-
-    Vec3 look_from(3.0, 3.0, 2.0);
-    Vec3 look_at(0.0, 0.0, -1.0);
+    HitableList world = random_scene();
+    
+    Vec3 look_from(12.0, 1.5, -4.0);
+    Vec3 look_at(0.0, 1.0, 0.0);
     float dist_to_focus = (look_from - look_at).norm();
-    float aperture = 2.0;
+    float aperture = 0.10;
     Camera cam(look_from, look_at, Vec3(0.0, 1.0, 0.0), 20.0, float(nx) / float(ny), aperture, dist_to_focus);
 
     for (int j = ny - 1; j >= 0; --j) {
